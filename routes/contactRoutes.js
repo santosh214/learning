fd830e03c2dbfa264793db5f47777c372821037f
santosh214 <santoshsingh74827@gmail.com>
2023-12-27 01:27:56 +0530

    add constact routes space

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 router.route('/').get(getContacts).post(createContact)
 router.route('/:id').get(getContact).put(updateContact).delete(deleteContact);
+
+
+
 module.exports = router;
